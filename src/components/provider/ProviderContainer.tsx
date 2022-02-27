31e3fe4f24b134b23fe56602562400f18cad7123
Nathan Folsom <n8folsom@gmail.com>
2022-02-27 11:24:54 -0800

    Adding remount button

--- src/components/provider/ProviderContainer.tsx
+++ src/components/provider/ProviderContainer.tsx
@@ -1,21 +1,49 @@
-import React, { PropsWithChildren } from "react";
+import React, { PropsWithChildren, useState } from "react";
 import styled from "styled-components";
 
 const Container = styled.div`
+  position: relative;
   text-align: center;
   background: white;
   flex: 1 1 0;
   padding: 15px;
   max-width: 300px;
-  margin: 5px;
+  margin: 10px;
+`;
+const RefreshButton = styled.button`
+  width: 20px;
+  height: 20px;
+  position: absolute;
+  top: -10px;
+  right: -10px;
+  border-radius: 10px;
+  border: none;
+  background: red;
+  
+  &:active {
+    opacity: .5;
+  }
+`;
+const PlaceHolder = styled.div`
+  background: whitesmoke;
+  width: 100%;
+  height: 20px;
 `;
 
 export type ProviderContainerProps = PropsWithChildren<any>;
 
 export default function ProviderContainer({ children }: ProviderContainerProps) {
+  const [render, setRender] = useState<boolean>(true);
+
+  const handleRemount = () => {
+    setRender(false);
+    setTimeout(() => setRender(true), 200);
+  }
+
   return (
     <Container>
-      {children}
+      <RefreshButton onClick={handleRemount}/>
+      {render ? children : <PlaceHolder />}
     </Container>
   );
 }
